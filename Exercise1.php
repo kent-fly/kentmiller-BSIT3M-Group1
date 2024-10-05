0ad75194a12c7728e40e50245b4217e2e16e7de7
kent-fly <kentmiller136@gmail.com>
2024-10-05 09:36:43 +0800

    Create Exercise1.php

---
+++ Exercise1.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Team</title>
+    <link href="tp.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<script>
+		window.alert("Do you want to refresh the page? ");
+	</script>
+	
+	<nav>
+		<div class="navbar">
+               <a href="home.php"><?php echo "Home" ?></a>
+               <class="nav"><a href="exercise1.php"><?php echo "Our Team" ?></a>
+			   <class="nav"><a href="certif.htm"><?php echo "Certificates" ?></a>
+			   <class="nav"><a href="port.htm"><?php echo "Portfolio" ?></a>
+			   <class="nav"><a href="project.htm"><?php echo "Our Project" ?></a>
+			   <class="nav"><a href="merch.php"><?php echo "Merch" ?></a>
+			   <class="nav"><a href="coms/contact.php"><?php echo "Contact Us" ?></a>
+ 			   <class="nav"><a href="Check.php"><?php echo "Check this out!!" ?></a>
+		</div>
+        
+	</nav>
+    
+    <div class="header">
+        <h1> <?php echo "Our Team Profile<br>BSIT-3M (Group 1)" ?> </h1>
+    </div>
+    <div class="container">
+        <div class="box">
+            <div class="member">
+                <img src="arlynne.jpg" alt="arlynne.">
+                <div class="overlay_ko">
+                    <div class="title"><?php echo "Arlynne T. Abalain <br> Role: Leader" ?></div>
+                    <p class="text"><?php echo "Life, is like a big toe"?></p>
+					<div class="icon-container">
+						<a class="fa fa-facebook" href="https://www.facebook.com/" target="_blank"></a>
+						<a class="fa fa-twitter" href="https://twitter.com/"target="_blank"></a>
+						<a class="fa fa-telegram" href="https://web.telegram.org/a/"target="_blank"></a>
+						<a class="fa fa-instagram" href="https://www.instagram.com/"target="_blank"></a>
+					</div>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="member">
+                <img src="baby.jpg" alt="angelica">
+                <div class="overlay_ko">
+                    <div class="title"><?php echo "Angelica D. Almaida <br> Role: Designer" ?></div>
+                    <p class="text"><?php echo "No Sense, Makes Sense" ?></p>
+					<div class="icon-container">
+						<a class="fa fa-facebook" href="https://www.facebook.com/" target="_blank"></a>
+						<a class="fa fa-twitter" href="https://twitter.com/"target="_blank"></a>
+						<a class="fa fa-telegram" href="https://web.telegram.org/a/"target="_blank"></a>
+						<a class="fa fa-instagram" href="https://www.instagram.com/"target="_blank"></a>
+					</div>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="member">
+                <img src="kent.jpg" alt="kentc">
+                <div class="overlay_ko">
+                    <div class="title"><?php echo "Kent Miller Catimbang<br> Role: Patutie" ?></div>
+                    <p class="text"><?php echo "GRavvvyyy" ?></p>
+                    <div class="icon-container">
+						<a class="fa fa-facebook" href="https://www.facebook.com/" target="_blank"></a>
+						<a class="fa fa-twitter" href="https://twitter.com/" target="_blank"></a>
+						<a class="fa fa-telegram" href="https://web.telegram.org/a/" target="_blank"></a>
+						<a class="fa fa-instagram" href="https://www.instagram.com/" target="_blank"></a>
+					</div>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="member">
+                <img src="cuano.jpg" alt="marvin">
+                <div class="overlay_ko">
+                    <div class="title"><?php echo "John Marvin Cuano <br> Role: Real Leader" ?></div>
+                    <p class="text"><?php echo "Addicted to the Pot" ?></p>
+                    <div class="icon-container">
+						<a class="fa fa-facebook" href="https://www.facebook.com/" target="_blank"></a>
+						<a class="fa fa-twitter" href="https://twitter.com/" target="_blank"></a>
+						<a class="fa fa-telegram" href="https://web.telegram.org/a/" target="_blank"></a>
+						<a class="fa fa-instagram" href="https://www.instagram.com/"target="_blank"></a>
+					</div>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="member">
+                <img src="jaren.jpg" alt="jaren">
+                <div class="overlay_ko">
+                    <div class="title"><?php echo "Jaren Rockwell Morante <br> Role: Organizer" ?></div>
+                    <p class="text"><?php echo "Ayeeeeeee" ?></p>
+                    <div class="icon-container">
+						<a class="fa fa-facebook"href="https://www.facebook.com/" target="_blank"></a>
+						<a class="fa fa-twitter" href="https://twitter.com/"target="_blank"></a>
+						<a class="fa fa-telegram" href="https://web.telegram.org/a/"target="_blank"></a>
+						<a class="fa fa-instagram" href="https://www.instagram.com/"target="_blank"></a>
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+	<hr>
+    
+	<footer>
+		<p style="margin-left:30px;color:#223030;"><small>Made by Abalain, Almaida, Catimbang, Cuaño & Morante 2024 © Copyright Intended</small></p>
+	</footer>
+
+    
+	</body>
+</html>
+
+<!-- Footer added via PHP -->
+<footer>Powered by PHP</footer>
+URAAA!!.
+URAAA!!
+URAAA!!
+URAAA!!
+URAAA!!
